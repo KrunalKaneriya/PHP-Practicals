a693f244977533d7f34e219e8ecdaddc2c5308dc
=Krunal Kaneria <=prokillerkk@outlook.com>
2021-08-04 08:18:54 +0530

    Created Cookies Project

--- PHP-Cookies/cookies.php
+++ PHP-Cookies/cookies.php
@@ -1,22 +1,32 @@
 <?php
-    $cookie_name = "user";
-    setcookie($cookie_name,"Krunal",time() + 86400);
+$cookie_name = "user";
+$cookie_name2 = "user2";
+setcookie($cookie_name, "Vidhi", time() + 86400);
+setcookie($cookie_name2,"Abhi");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Cookie</title>
 </head>
+
 <body>
     <?php
-        //we will see that the cookie is set or not 
-        if(!isset($_COOKIE[$cookie_name])) {
-            echo "Cookie Named " . $cookie_name . "is not set.";
-        } else {
-            echo "Cookie Named " . $cookie_name . " is set.";
-            echo "Cookie Value is " . $_COOKIE[$cookie_name];
-        }
+    //we will see that the cookie is set or not 
+    if ((!isset($_COOKIE[$cookie_name])) && (!isset($_COOKIE[$cookie_name2]))) {
+        echo "Cookie Named " . $cookie_name . " is not set.";
+        echo "<br>Cookie Named ". $cookie_name2 . " is not set.";
+    } else {
+        echo "Cookie Named " . $cookie_name . " is set.";
+        echo "<br>Cookie named "  . $cookie_name2 . " is set.";
+        echo "<br>Cookie Value is " . $_COOKIE[$cookie_name];
+        echo "<br>Cookie value is  " .$_COOKIE[$cookie_name2];
+    }
     ?>
+
+        
 </body>
+
 </html>
