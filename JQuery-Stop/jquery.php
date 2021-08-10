c948b1ef60fa2a7569afc3e8ae34b7d6de709f1e
=Krunal Kaneria <=prokillerkk@outlook.com>
2021-08-10 18:45:32 +0530

    Added Stop Parameterized Function

--- JQuery-Stop/jquery.php
+++ JQuery-Stop/jquery.php
@@ -12,6 +12,17 @@
 
     <button id = "start">Start The Animation</button>
     <button id = "stop">Stop The Animation</button>
+
+    <br><br><br>
+
+    <div id = "another-div" style = "background-color:orange; width:300px;height:200px;position:relative;">
+        Hello World
+    </div>
+
+    <button id = "start2">Start The Animation</button>
+    <button id = "stop1">Stop The Single Animation</button><br>
+    <button id = "stop2">Stop All The Animation But Don't Finish</button>
+    <button id = "stop3">Stop All The Animations And Finish It</button>
 <script>
    $(document).ready(function() {
     $("#start").click(function() {
@@ -20,6 +31,27 @@
 
     $("#stop").click(function () {
         $("#panel").stop(false,true);
+    });
+
+    $("#start2").click(function() {
+        $("#another-div").animate({left: '500px'}, 5000);
+        $("#another-div").animate({'font-size': "3em"} ,5000);
+    });
+
+    $("#stop1").click(function() {
+        $("#another-div").stop();
+    });
+
+    $("#stop2").click(function() {
+        $("#another-div").stop(true);
+    });
+
+    $("#stop3").click(function() {
+        $("#another-div").stop(true,true);
+    });
+
+    $("#stop").click(function() {
+        $("#panel").stop();
     });
 
    });
